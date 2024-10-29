ed75c235bed03556fc2fdd28d9ca5d9cc9b9083e
djallal rahmoune <djallalrahmoune@gmail.com>
2024-10-29 17:09:51 +0100

    img icon added on canvas ok

--- main.py
+++ main.py
@@ -3,35 +3,29 @@ from tkinter import ttk, filedialog
 from PIL import ImageTk, Image
 
 
-""" 
+IMAGEUPLOADED = ""
+
+
 def uploadImg(h):
-    im = Image.open(filedialog.askopenfilename())
-    im = im.resize((h,h))
-    img = PhotoImage(im) 
-    return img
+    lien = filedialog.askopenfilename()
+    img_Image = Image.open(lien)
+    img_Image = img_Image.resize((h,h))
+    imge = ImageTk.PhotoImage(img_Image)
+    return imge
 
 def add_logo_pc():
-
-    img =filedialog.askopenfilename()
-    print(img)
-    panel1 = Label(fr_canvas, image=img)
-    panel1.image = img
-    panel1.pack()
+    print("addIcon clicked")
+    canvas.itemconfig(canvas_icon, image=icon)
 
 def add_img_pc():
+    print("addImgPc clicked")
+    canvas.itemconfig(canvas_image, image=imge)
     
-    #img1 = filedialog.askopenfilename()#uploadImg(int(h/8))
-    #print(img1)
-    canva_image = Canvas(fr_canvas, width=w/3, height=h/3,bg="white")
+
     
-    imge = PhotoImage(file="instagram.png")
-    #canva_image.create_image(h/3,w/3,image=imge)
-    canva_image.create_text(h/3,w/3,text="hhh", fill="black", font="Times 20 italic bold")
-    canva_image.pack() 
-
-
-    btn_logo = ttk.Button(fr_btn, text="Add Logo from PC", command=add_logo_pc).pack() 
- """
+ 
+    
+ 
 
 
 #global GUI
@@ -44,18 +38,34 @@ x = (ws/2) - (w/2)
 y = (hs/2) - (h/2)
 root.geometry('%dx%d+%d+%d' % (w-350, h, x+150, y))
 root.resizable(False,False)
-
+#root.after(1000, func=add_img_pc)
 
 #buttom pallete
 fr_btn = ttk.Frame(root, padding=60)
 fr_btn.pack()
 
-btn_img = ttk.Button(fr_btn, text="Add Img from PC", command="")
-btn_icon = ttk.Button(fr_btn, text="Add Icon from PC", command="")
+#filedialog.askopenfilename()
+img_Image = Image.open('avatar.jpg')
+img_Image = img_Image.resize((h-20,h-210))
+imge = ImageTk.PhotoImage(img_Image)
+
+img_Image1 = Image.open('instagram.png')
+img_Image2 = img_Image1.resize((h-20,h-210))
+icon = ImageTk.PhotoImage(img_Image2)
+
+
+
+btn_img = ttk.Button(fr_btn, text="Add Img from PC", command=add_img_pc)
+btn_icon = ttk.Button(fr_btn, text="Add Icon from PC", command=add_logo_pc)
 btn_save = ttk.Button(fr_btn, text="Save", command="")
 label = Label(fr_btn,text="")
 label1 = Label(fr_btn,text="")
-canvas = ttk.Frame(fr_btn, width=800, height=600,borderwidth=5, relief="ridge")
+canvas = Canvas(fr_btn, width=800, height=600,borderwidth=5, relief="ridge")
+canvas_image = canvas.create_image(0,0, anchor=NW, image="")
+canvas_icon = canvas.create_image(50,50, anchor=NW, image="")
+
+
+
 
 btn_img.grid(column=0, row=1)
 btn_icon.grid(column=2, row=1)
@@ -67,3 +77,4 @@ canvas.grid(column=0,row=4, columnspan=5)
 
 
 root.mainloop()
+
